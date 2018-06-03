4d6ace7b689b01a8a4346b3569d6036ed445c364
Paula <paulamiszcz@wp.pl>
2018-06-03 22:02:08 +0200

    Suma poszczególnych liczb z tablicy

---
+++ src/Tablica.java
@@ -0,0 +1,7 @@
+public class Tablica {
+    public static void main(String[] args){
+        int[] tablica = new int[] {8,5,3,1,4};
+        System.out.println(tablica[0] + tablica[2] + tablica [4]);
+
+    }
+}
